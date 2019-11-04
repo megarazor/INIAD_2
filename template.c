e2ba267e3459fabc8bf50945e4fa0bd1b3d8d9f8
megarazor <vannampham1301@gmail.com>
2019-11-05 01:17:18 +0900

    Update

--- template.c
+++ template.c
@@ -31,5 +31,28 @@ int main ()
         close(pipefd[1]);
         close(STDIN_FILENO);
         dup2(pipefd[0], STDIN_FILENO);
+
+        int pipefd_2[2];
+        pipe(pipefd_2);
+        if(pipe(pipefd_2) != 0){
+            fprintf(stderr, "pipe(2) failed.\n");
+            return 1;
+        }
+        pid_t pid_2= fork();
+        if (pid_2 < 0){
+            printf("Error\n");
+            return 1;
+        }
+        else if (pid_2 > 0){
+            close(pipefd_2[0]);
+            close(STDOUT_FILENO);
+            dup2(pipefd[1], STDOUT_FILENO);
+
+            int status;
+            waitpid(pid, &status, 0);
+        }
+        else {
+
+
     }
 }
